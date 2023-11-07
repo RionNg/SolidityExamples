16cef8230b74afde1cc6917e18603db5770afffe
RionNg <147371006+RionNg@users.noreply.github.com>
2023-11-07 13:04:36 +0800

    3_Arrays.sol

--- Solidity Examples/3_Arrays.sol
+++ Solidity Examples/3_Arrays.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 
 contract MyContract {
     // Arrays
-    // index = [0, 1, 2]
+    // index = 0, 1, 2
     // element = [11, 23, 55]
-    // therefore = [0 = 11, 1 = 23, 2 = 55]
+    // therefore = 0 = 11, 1 = 23, 2 = 55
     uint256[] public uint256Array = [1, 2, 3];
     string[] public stringArray = ["a", "b", "c"];
     string[] public values;
